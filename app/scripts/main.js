f0813b56788533924400f8e041cf2e782eeb7687
Jennifer Meier <meierjl01@gmail.com>
2016-10-25 09:08:32 -0500

    added file to stop errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,3 +6,18 @@ import Message from './message';
 import renderLogin from './login';
 import renderChat from './chat';
 import renderSendMessage from './sendmessage';
+
+
+function handleHashChange(e) {
+  let location = window.location.hash;
+  if (location === '') {
+    renderLogin();
+  } else if (location === '#chat') {
+    //empty here or in render function?
+    $('.header').empty();
+    $('.chat-area').empty();
+    renderChat();
+  }
+}
+
+$(window).on('hashchange', handleHashChange);
