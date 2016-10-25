13eacc70744b8f24724e28cf6d7918ec996563cd
Jennifer Meier <meierjl01@gmail.com>
2016-10-24 19:10:24 -0500

    added other js files

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import _ from 'underscore';
+
+import Session from './session';
+import Message from './message';
+import renderLogin from './login';
+import renderChat from './chat';
+import renderSendMessage from './sendmessage';
